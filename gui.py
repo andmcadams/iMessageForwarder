ff264f3c3399a6f2eb170f5218bbe9463d0b9a6a
Andrew McAdams <andmcadams@gmail.com>
2020-01-14 08:46:09 -0600

    Make send button activate when there is text in the text box and deactivate when there is none

--- gui.py
+++ gui.py
@@ -155,18 +155,41 @@ class RecipientFrame(tk.Frame):
         self.label.configure(text=recipString, wraplength=int(0.66*self.winfo_width()))
         self.details.configure(text='Details')
 
+class ResponsiveText(tk.Text):
+    def __init__(self, parent, *args, **kw):
+        tk.Text.__init__(self, parent, *args, **kw)
+
+        self._orig = self._w + '_orig'
+        self.tk.call('rename', self._w, self._orig)
+        self.tk.createcommand(self._w, self._proxy)
+
+    def _proxy(self, command, *args):
+        cmd = (self._orig, command) + args
+        result = self.tk.call(cmd)
+
+        if command in ('insert', 'delete', 'replace'):
+            self.event_generate("<<TextModified>>")
+
+        return result
+
 class SendFrame(tk.Frame):
     def __init__(self, parent, *args, **kw):
         tk.Frame.__init__(self, parent, *args, **kw)
-        self.text = tk.Text(self, wrap=tk.WORD, width=1, height=1)
+        self.text = ResponsiveText(self, wrap=tk.WORD, width=1, height=1)
         self.text.grid(row=0, column=0, sticky='ew')
-
+        self.text.bind("<<TextModified>>", self.activateButton)
         self.columnconfigure(0, weight=1)
 
         self.sendButton = tk.Button(self, relief=tk.FLAT, 
             bg="gray99", fg="purple3",
-            font="Dosis", text='Send')
+            font="Dosis", text='Send', state='disabled')
         self.sendButton.grid(row=0, column=1)
+
+    def activateButton(self, event):
+        if not self.text.compare("end-1c", "==", "1.0"):
+            self.sendButton.configure(state='normal')
+        else:
+            self.sendButton.configure(state='disabled')
 
     def sendMessage(self, chat):
         chat.sendMessage(self.text.get('1.0', 'end-1c'))
