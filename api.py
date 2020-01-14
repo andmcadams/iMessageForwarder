a696ab4dc839071266ca99f78caf670a47a5d01b
Andrew McAdams <andmcadams@gmail.com>
2020-01-14 09:53:34 -0600

    Let message constructor take in list of args to avoid brittleness

--- api.py
+++ api.py
@@ -1,8 +1,10 @@
 import sqlite3
 import os
 import json
+import time
 
 conn = sqlite3.connect('../messages/SMS/sms.db')
+conn.row_factory = sqlite3.Row
 
 secrets = json.load(open('secrets.json'))
 ip = secrets['ip']
@@ -28,8 +30,8 @@ class MessageList(dict):
 
 		# If the message is just being updated, no need to worry about ordering
 		# (assumption that date should never change)
-		if message.messageId in updatedMessages:
-			updatedMessages[message.messageId] = message
+		if message.attr['ROWID'] in updatedMessages:
+			updatedMessages[message.attr['ROWID']] = message
 		else:
 			keys = list(updatedMessages)
 			# Here we need to check the dates near the end of the dictionary
@@ -39,7 +41,7 @@ class MessageList(dict):
 				# Find first message older than the new message
 				i = -1
 				for i in range(-1, -len(keys)):
-					if message.date > updatedMessages[key].date:
+					if message.attr['date'] > updatedMessages[key].attr['date']:
 						break
 				# i contains the index that message should be inserted after
 				# Note that if i == -1, we don't need to remove anything since -1 is the last index
@@ -51,32 +53,33 @@ class MessageList(dict):
 					for key in keysToRemove:
 						poppedMessages.append(updatedMessages.pop(key))
 					# Add in the new message
-					updatedMessages[message.messageId] = message
+					updatedMessages[message.attr['ROWID']] = message
 					# Add back the messages we removed from the list
 					for poppedMessage in poppedMessages:
 						p = poppedMessages.pop()
-						updatedMessages[p.messageId] = p
+						updatedMessages[p.attr['ROWID']] = p
 				else:
-					updatedMessages[message.messageId] = message
+					updatedMessages[message.attr['ROWID']] = message
 			# If the list is empty we end up here
 			else:
-				updatedMessages[message.messageId] = message
+				updatedMessages[message.attr['ROWID']] = message
 		self.messages = updatedMessages
 
 class Message:
 
-	def __init__(self, messageId, handleId, text, date, isFromMe):
-		self.messageId = messageId
-		self.handleId = handleId
-
-		self.date = date
-		self.isFromMe = isFromMe
+	def __init__(self, **kw):
+		self.attr = {}
+		for key, value in kw.items():
+			self.attr[key] = value
 
 		# This has to be done since tkinter only supports some unicode characters
-		self.text = None
-		if text != None:
-			self.text = ''.join([text[t] for t in range(len(text)) if ord(text[t]) in range(65536)])
+		self.attr['text'] = None
+		if kw['text'] != None:
+			self.attr['text'] = ''.join([kw['text'][t] for t in range(len(kw['text'])) if ord(kw['text'][t]) in range(65536)])
 
+class DummyChat:
+	def __init__(self, chatId):
+		self.chatId = chatId
 
 class Chat:
 
@@ -88,6 +91,7 @@ class Chat:
 		self.messageList = MessageList()
 		self.mostRecentMessage = self._getMostRecentMessage()
 		self.recipientList = self._loadRecipients()
+		self.lastAccess = 0
 
 	def _loadRecipients(self):
 		cursor = conn.execute('select id from handle inner join chat_handle_join on handle.ROWID = chat_handle_join.handle_id and chat_handle_join.chat_id = ?', (self.chatId, ))
@@ -99,7 +103,7 @@ class Chat:
 	def _getMostRecentMessage(self):
 		cursor = conn.execute('select ROWID, handle_id, text, max(message.date), is_from_me from message inner join chat_message_join on message.ROWID = chat_message_join.message_id and chat_message_join.chat_id = ?', (self.chatId, ))
 		m = cursor.fetchone()
-		message = Message(*m)
+		message = Message(**m)
 
 		# If I can guarantee that self.messages[-1] is the most recent message, this can be a lot simpler
 		return message
@@ -115,15 +119,27 @@ class Chat:
 
 	def _loadMessages(self):
 		# if self.messages is not none, we should just query for messages received after last message
-		cursor = conn.execute('select ROWID, handle_id, text, date, is_from_me from message inner join chat_message_join on message.ROWID = chat_message_join.message_id and chat_message_join.chat_id = ?', (self.chatId, ))
+		print(self.lastAccess)
+		tempLastAccess = int(time.time())
+		neededColumnsMessage = ['ROWID', 'guid', 'text', 'handle_id', 'service', 'error', 'date', 'date_read', 'date_delivered', 'is_delivered', 'is_finished', 'is_from_me', 'is_read', 'is_sent', 'cache_has_attachments', 'cache_roomnames', 'item_type', 'other_handle', 'group_title', 'group_action_type', 'associated_message_guid', 'associated_message_type']
+
+		columns = ', '.join(neededColumnsMessage)
+		sql = 'SELECT {} FROM message inner join chat_message_join on message.ROWID = chat_message_join.message_id and (date > ? or date_read > ? or date_delivered > ?) and chat_message_join.chat_id = ? '.format(columns)
+		cursor = conn.execute(sql, (self.lastAccess, self.lastAccess, self.lastAccess, self.chatId))
+
 		for row in cursor:
-			message = Message(*row)
+			message = Message(**row)
 			self.messageList.append(message)
+
+		self.setAccessTime(tempLastAccess)	
 
 	def sendMessage(self, messageText):
 		messageText = messageText.replace('\"', '\\\"')
 		cmd = "ssh root@{} \"python {} \\\"{}\\\" {}\"".format(ip, scriptPath, messageText, self.chatId)
 		os.system(cmd)
+
+	def setAccessTime(self, t):
+		self.lastAccess = t - 978307200
 
 
 def _loadChats():
@@ -131,6 +147,6 @@ def _loadChats():
 	chats = []
 	for row in cursor.fetchall():
 		chat = Chat(row[0], row[1], row[2])
-		if chat.mostRecentMessage.messageId != None:
+		if chat.mostRecentMessage.attr['ROWID'] != None:
 			chats.append(chat)
 	return chats
