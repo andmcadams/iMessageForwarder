a1a860a797cf6db6aa015931ec2d5c21341a2c20
Andrew McAdams <andmcadams@gmail.com>
2020-01-15 14:08:40 -0600

    Clean up the ChatButton class

--- gui.py
+++ gui.py
@@ -92,36 +92,53 @@ class VerticalScrolledFrame(tk.Frame):
 class ChatButton(tk.Frame):
     def __init__(self, parent, chat, responseFrame, *args, **kw):
         tk.Frame.__init__(self, parent, *args, **kw)
-        self.chat = chat
-        self.picture = tk.Label(self, height=1, width=1, bg='red', text='picture')
-        name = chat.getName()
-        if name and len(name) > 20:
-            name = name[0:17] + '...'
-        self.number = tk.Label(self, height=1, width=1, bg='blue', anchor='nw', justify='left', font=("helvetica", 10), text=name, wraplength=0)
-        text = chat.getMostRecentMessage().attr['text']
-        if text and len(text) > 50:
-            text = text[0:50] + '...'
-        self.lastMessage = tk.Label(self, height=2, width=1, bg='green', anchor='nw', justify='left', text=text, font=("helvetica", 10), wraplength=200)
-   
 
-        timeText = self.getTimeText(chat.getMostRecentMessage().attr['date'])
-        self.lastMessageTime = tk.Label(self, height=1, width=1, bg='yellow', anchor='se', justify='right', font=("helvetica", 8), text=timeText)
+        self.chat = chat
         self.lastMessageId = chat.getMostRecentMessage().attr['ROWID']
+
+        self.picture = tk.Label(self, height=1, width=1, text='picture')
+        self.number = tk.Label(self, height=1, width=1, anchor='nw', justify='left', font=("helvetica", 10), wraplength=0)
+        self.lastMessage = tk.Label(self, height=2, width=1, anchor='nw', justify='left', font=("helvetica", 10), wraplength=200)
+        self.lastMessageTime = tk.Label(self, height=1, width=1, anchor='se', justify='right', font=("helvetica", 8))
+
+        # Populate the above Label widgets
+        self.update()
+
+        # For debugging purposes, recolor the backgrounds of each label
+        self.picture.configure(bg='red')
+        self.number.configure(bg='blue')
+        self.lastMessage.configure(bg='green')
+        self.lastMessageTime.configure(bg='yellow')
+        # This can be removed at any time.
+
         self.picture.grid(row=0, column=0, rowspan=2, sticky='nsew')
         self.number.grid(row=0, column=1, sticky='nsew')
         self.lastMessage.grid(row=1, column=1, columnspan=2, sticky='nsew')
         self.lastMessageTime.grid(row=0, column=2, sticky='nsew')
+
+
         self.columnconfigure(0, weight=2)
         self.columnconfigure(1, weight=5)
+        self.columnconfigure(2, weight=2)
         self.rowconfigure(0, weight=1)
         self.rowconfigure(1, weight=1)
-        self.columnconfigure(2, weight=2)
 
-        self.bind('<Button-1>', lambda event, chat=chat: responseFrame.changeChat(chat))
-        self.lastMessageTime.bind('<Button-1>', lambda event, chat=chat: responseFrame.changeChat(chat))
-        self.picture.bind('<Button-1>', lambda event, chat=chat: responseFrame.changeChat(chat))
-        self.lastMessage.bind('<Button-1>', lambda event, chat=chat: responseFrame.changeChat(chat))
-        self.number.bind('<Button-1>', lambda event, chat=chat: responseFrame.changeChat(chat))
+        # Set up bindings so that clicking on any part of the chat button will open up the chat.
+        onClick = lambda event, chat=chat: responseFrame.changeChat(chat)
+        self.bind('<Button-1>', onClick)
+        self.picture.bind('<Button-1>', onClick)
+        self.number.bind('<Button-1>', onClick)
+        self.lastMessage.bind('<Button-1>', onClick)
+        self.lastMessageTime.bind('<Button-1>', onClick)
+
+    def truncate(self, string, length):
+        # Use lstrip to remove any leading newlines, which could cause some issues.
+        # Note that using lstrip here ONLY affects what the text looks like in the chat button.
+        # Use rstrip to remove awkward space between text and ellipsis in case last char is a space
+        if string:
+            string = string.lstrip()
+            return (string[:length-3].rstrip() + '...') if len(string) > length else string
+        return ''
 
     def getTimeText(self, timeStamp):
         currentTime = datetime.now(tz=datetime.now().astimezone().tzinfo)
@@ -135,18 +152,16 @@ class ChatButton(tk.Frame):
         return timeText  
 
     def update(self):
-        text = self.chat.getMostRecentMessage().attr['text']
         self.lastMessageId = self.chat.getMostRecentMessage().attr['ROWID']
-        if text and len(text) > 50:
-            text = text[0:50] + '...'
-        self.lastMessage.configure(text=text)
+
+        name = self.truncate(self.chat.getName(), 20)
+        text = self.truncate(self.chat.getMostRecentMessage().attr['text'], 50)
         timeText = self.getTimeText(self.chat.getMostRecentMessage().attr['date'])
+
+        self.number.configure(text=name)
+        self.lastMessage.configure(text=text)
         self.lastMessageTime.configure(text=timeText)
 
-        name = chat.getName()
-        if name and len(name) > 20:
-            name = name[0:17] + '...'
-        self.number.configure(text=name)
 
 
 class ChatFrame(VerticalScrolledFrame):
