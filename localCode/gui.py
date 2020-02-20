ab77529ec01d598709c246cb1ffc273316e506a3
Andrew McAdams <andmcadams@gmail.com>
2020-02-20 14:20:17 -0600

    Handle deleted conversation by catching custom exception

--- localCode/gui.py
+++ localCode/gui.py
@@ -344,33 +344,38 @@ class ResponseFrame(tk.Frame):
 def updateFrames(chatFrame, responseFrame):
     chatIds = api._getChatsToUpdate(0)
     for chatIdTuple in chatIds:
-        chatId = chatIdTuple[0]
-        if chatId == responseFrame.currentChat.chatId:
-            responseFrame.messageFrame.addMessages(responseFrame.currentChat)
-        # If there's a change in the number of messages in this chat
-        # push the chat to the top
-        if chatId in chatFrame.chatButtons:
-            oldMessageId = chatFrame.chatButtons[chatId].lastMessageId
-            chatFrame.chatButtons[chatId].chat._loadMostRecentMessage()
-            mostRecent = chatFrame.chatButtons[chatId].chat.getMostRecentMessage()
-            if oldMessageId != mostRecent.attr['ROWID']:
-                # Push this chat to the top
+        try:
+            chatId = chatIdTuple[0]
+            if chatId == responseFrame.currentChat.chatId:
+                responseFrame.messageFrame.addMessages(responseFrame.currentChat)
+            # If there's a change in the number of messages in this chat
+            # push the chat to the top
+            if chatId in chatFrame.chatButtons:
+                oldMessageId = chatFrame.chatButtons[chatId].lastMessageId
+                chatFrame.chatButtons[chatId].chat._loadMostRecentMessage()
+                mostRecent = chatFrame.chatButtons[chatId].chat.getMostRecentMessage()
+                if oldMessageId != mostRecent.attr['ROWID']:
+                    # Push this chat to the top
+                    for rowid in chatFrame.chatButtons:
+                        chatFrame.chatButtons[rowid].pack_forget()
+                    chatFrame.chatButtons[chatId].update()
+                    chatFrame.chatButtons[chatId].pack(fill=tk.X, side=tk.TOP, pady=1)
+                    for rowid in chatFrame.chatButtons:
+                        if rowid != chatId:
+                            chatFrame.chatButtons[rowid].pack(fill=tk.X, side=tk.TOP, pady=1)
+            else:
+                chat = api._loadChat(chatId)
                 for rowid in chatFrame.chatButtons:
                     chatFrame.chatButtons[rowid].pack_forget()
-                chatFrame.chatButtons[chatId].update()
-                chatFrame.chatButtons[chatId].pack(fill=tk.X, side=tk.TOP, pady=1)
+                chatFrame.addChat(chat, responseFrame)
                 for rowid in chatFrame.chatButtons:
-                    if rowid != chatId:
+                    if rowid != chatId:            
                         chatFrame.chatButtons[rowid].pack(fill=tk.X, side=tk.TOP, pady=1)
-        else:
-            chat = api._loadChat(chatId)
-            for rowid in chatFrame.chatButtons:
-                chatFrame.chatButtons[rowid].pack_forget()
-            chatFrame.addChat(chat, responseFrame)
-            for rowid in chatFrame.chatButtons:
-                if rowid != chatId:            
-                    chatFrame.chatButtons[rowid].pack(fill=tk.X, side=tk.TOP, pady=1)
-            #need to reconfigure scrollbars again
+                #need to reconfigure scrollbars again
+        except api.ChatDeletedException as e:
+            # Probably want to delete the messages to avoid unnecessary computation.
+            # But should deleting the chat on another device delete it from here?
+            continue
             
         # 
     threading.Timer(1, lambda chatFrame=chatFrame, responseFrame=responseFrame: updateFrames(chatFrame, responseFrame)).start()
