cd32e23cbeacd271bbac37881d530cddce7655e1
Andrew McAdams <andmcadams@gmail.com>
2020-05-26 07:57:29 -0500

    Terminate both threads when killing the parent in the console

--- localCode/startup.py
+++ localCode/startup.py
@@ -1,10 +1,15 @@
 import threading
+import signal
 import time
 import updater
 import gui
 
 if __name__ == '__main__':
-	t1 = threading.Thread(target=updater.runUpdater)
-	t1.start()
-	t2 = threading.Thread(target=lambda debug=0: gui.runGui(debug))
-	t2.start()
+    def terminateThreads(signalNumber, frame):
+        t1.stopThread()
+    signal.signal(signal.SIGINT, terminateThreads)
+    t1 = updater.UpdaterThread()
+    t1.start()
+    t2 = threading.Thread(target=lambda debug=0: gui.runGui(debug))
+    t2.daemon = True
+    t2.start()
