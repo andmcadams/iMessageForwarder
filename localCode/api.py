7068e014f36ea917c85dff41a35f1574a703bac6
Andrew McAdams <andmcadams@gmail.com>
2020-05-31 00:47:20 -0500

    Create a new exception that's raised when attempting to create a Message object without a ROWID param in kw

--- localCode/api.py
+++ localCode/api.py
@@ -94,6 +94,8 @@ class Attachment:
 		for key, value in kw.items():
 			self.attr[key] = value
 
+class MessageNoIdException(Exception):
+	pass
 
 class Message:
 
@@ -103,6 +105,8 @@ class Message:
 		for key, value in kw.items():
 			self.attr[key] = value
 
+		if not 'ROWID' in self.attr:
+			raise MessageNoIdException
 		# This has to be done since tkinter only supports some unicode characters
 		self.attr['text'] = None
 		if 'text' in kw and kw['text'] != None:
