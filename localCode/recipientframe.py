9ed7d643de4bfe7ae4a38113256fd197df1b1c24
Andrew McAdams <andmcadams@gmail.com>
2020-06-03 08:58:35 -0500

    Remove various debug bg colors

--- localCode/recipientframe.py
+++ localCode/recipientframe.py
@@ -7,11 +7,9 @@ class RecipientFrame(tk.Frame):
         self.labelFrame = None
 
         self.andMore = tk.Label(self, text='', anchor='e', justify=tk.LEFT)
-        self.andMore.configure(bg='gray')
         self.andMore.grid(row=0, column=1, padx=(5, 0), sticky='se')
 
         self.details = tk.Label(self, text='', anchor='e', justify=tk.LEFT)
-        self.details.configure(bg='red')
         self.details.grid(row=0, column=2, sticky='se')
 
         self.columnconfigure(0, weight=1)
@@ -31,13 +29,10 @@ class RecipientLabelFrame(tk.Frame):
 
     def __init__(self, parent, chat, *args, **kw):
         tk.Frame.__init__(self, parent, *args, **kw)
-        self.configure(bg='green')
         self.topFrame = RecipientLabelSubframe(self)
-        self.topFrame.configure(bg='blue')
         self.topFrame.pack(expand=True, fill=tk.BOTH)
         self.topFrame.columnconfigure(0, weight=1)
         self.bottomFrame = RecipientLabelSubframe(self)
-        self.bottomFrame.configure(bg='orange')
         self.bottomFrame.pack(expand=True, fill=tk.BOTH, side=tk.BOTTOM)
         self.topSize = 0
         self.bottomSize = 0
@@ -189,7 +184,6 @@ class RecipientLabelSubframe(tk.Frame):
 
     def __init__(self, parent, *args, **kw):
         tk.Frame.__init__(self, parent, *args, **kw)
-        self.configure(bg='blue')
         self.pack_propagate(0)
         self.labels = []
 
