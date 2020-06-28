617e58b7aeef9f5e0546ebd7bebf07a831a5a178
Andrew McAdams <andmcadams@gmail.com>
2020-06-28 16:21:13 -0500

    Add simple test for Received object

---
+++ tests/testReceived.py
@@ -0,0 +1,10 @@
+import unittest
+from localCode import api
+
+
+class TestReceivedMethods(unittest.TestCase):
+
+    # Received is an abstract class and should not be instantiated.
+    def test_fail_to_construt(self):
+        with self.assertRaises(TypeError):
+            api.Received()
